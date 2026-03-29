ed50af91fef55222a69204cc303b66f6e214f225
ArthurInfo26 <arthurguifran@gmail.com>
2026-03-29 11:22:19 -0300

    Add files via upload

---
+++ aulas-do-gil/10mar/31mar/ex07.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int  v1, cent, dez, unit, resto;
+
+    printf("Lados dos triangulos: ");
+    scanf("%i", &v1);
+    if(v1 >= 111 && v1 <= 999){
+        resto = v1;
+    
+        cent = resto /100;
+        dez = (resto %100) /10;
+        unit = (resto %100) %10;
+    
+    
+        if(cent == dez && dez == unit){
+            printf("Equilatero");
+    
+        }
+    
+        else if(cent != dez && dez != unit && unit != cent){
+            printf("Escaleno");
+        
+        }
+        else if(cent != dez && cent == unit || cent != unit && cent == dez || dez!= cent && dez == unit){
+            printf("Isosceles");
+    
+        }
+    }
+    else{
+        printf("bota valor certo");
+    }
+
+    return 0;
+}
